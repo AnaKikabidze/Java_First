fa5252f454b268724f22549299eb19f6f977fc42
AnaKikabidze <anastasiyagbm@gmail.com>
2023-03-17 22:00:30 +0500

    second_commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const salaryСleaner = 40000;
+const salaryTeacher = 22000;
+const cleanerTeacher = salaryСleaner + salaryTeacher;
+console.log(
+  `Вечерний уборщик, имея голос и педагогическое образование зарабатывает ${cleanerTeacher} рублей`
+);
+
+const stringCity = "Владивосток";
+const Time = 2000;
+console.log(`${stringCity} ${Time}`);
+
+const chattingTime = 128;
+const likeTime = 254;
+const vkReadArticles = 137;
+const watchYoutube = 201;
+const mermaidTogether = chattingTime + likeTime + vkReadArticles + watchYoutube;
+const minutesInHour = 60;
+const timeInHours = mermaidTogether / minutesInHour;
+console.log(`Ответ:Русалка страдала фигней целых, ${timeInHours} часов`);
+
+const result = 5 ** 2;
+console.log(`Если возвести число 2 в 5 степень, то получиться ${result}`);
+
+let message =
+  "Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!";
+console.log(message);
+
+const greetings = "Привет";
+const name = "Кот";
